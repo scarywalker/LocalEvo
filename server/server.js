7750a0082d4eea3cdc4820108d3223b0d5731a64
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-12-06 00:19:13 +0200

    a

--- server/server.js
+++ server/server.js
@@ -41,16 +41,35 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
 app.post("/api/v1/restaurants", async (req, res) => {
   try {
     const results = await db.query(
-      "INSERT INTO restaurants (name, location, price_range) values ($1,$2,$3)",
-      [req.params.id, req.params.id, req.params.id]
+      "INSERT INTO restaurants (name, location, price_range) values ($1,$2,$3) returning *",
+      [req.body.name, req.body.location, req.body.price_range]
     );
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      data: { restaurant: results.rows[0] },
+    });
   } catch (error) {
     console.log(error);
   }
 });
 
-// app.put("/api/v1/restaurants/:id", (req, res) => {});
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const results = await db.query(
+      "UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    );
+    console.log(results);
+    res.status(201).json({
+      status: "success",
+      data: { restaurant: results.rows[0] },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-// app.delete("/api/v1/restaurants/:id", (req, res) => {});
+app.delete("/api/v1/restaurants/:id", (req, res) => {});
 
 app.listen(port, () => console.log(`server on : ${port}`));
