28df8b96108cb4a628cb2d89dd2a537b59c5c8d2
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-12-05 20:39:49 +0200

    a

---
+++ server/db/index.js
@@ -0,0 +1,10 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+module.exports = { query: (text, params) => pool.query(text, params) };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const db = require("./db");
 
 const port = process.env.PORT || 3002;
 
@@ -8,27 +9,48 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/api/v1/restaurants", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: { restaurant: ["mcdonalds", "wendys"] },
-  });
+app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query("SELECT * FROM restaurants;");
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: { restaurant: results.rows },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.get("/api/v1/restaurants", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: { restaurant: ["mcdonalds", "wendys"] },
-  });
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const results = await db.query("SELECT * FROM restaurants WHERE id = $1;", [
+      req.params.id,
+    ]);
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      data: { restaurant: results.rows[0] },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.post("/api/v1/restaurants", (req, res) => {
+app.post("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query(
+      "INSERT INTO restaurants (name, location, price_range) values ($1,$2,$3)",
+      [req.params.id, req.params.id, req.params.id]
+    );
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.put("/api/v1/restaurants/:id", (req, res) => {
-});
+// app.put("/api/v1/restaurants/:id", (req, res) => {});
 
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-});
+// app.delete("/api/v1/restaurants/:id", (req, res) => {});
 
 app.listen(port, () => console.log(`server on : ${port}`));
