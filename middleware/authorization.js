f4de19572eaed6d6f0db5f201cf8045957f8eadb
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-12-19 14:40:42 +0200

    a

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -7,7 +7,7 @@ module.exports = async (req, res, next) => {
 
     if (!jwtToken) return res.status(403).json("Not Authorized");
 
-    const payload = jwt.verify(jwtToken, process.env.jwtSecret);
+    const payload = jwt.verify(jwtToken, process.env.jwtSecret || potato);
 
     req.user = payload.user;
 
